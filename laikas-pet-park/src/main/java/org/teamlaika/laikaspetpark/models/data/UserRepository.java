2840077340f5f5f2053d12a680558d1e57f92f02
terryweis <weist2088@gmail.com>
2024-03-28 17:26:59 -0500

    UserRepo upgraded

--- laikas-pet-park/src/main/java/org/teamlaika/laikaspetpark/models/data/UserRepository.java
+++ laikas-pet-park/src/main/java/org/teamlaika/laikaspetpark/models/data/UserRepository.java
@@ -1,8 +1,10 @@
 package org.teamlaika.laikaspetpark.models.data;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import org.teamlaika.laikaspetpark.models.User;
 
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
 
     User findByUsername(String username);
