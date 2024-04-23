44732dee7d838ccb4335d26acd38ec24039d2b56
terryweis <weist2088@gmail.com>
2024-04-23 13:05:25 -0500

    JWT

---
+++ laikas-pet-park/src/main/java/org/teamlaika/laikaspetpark/JwtGenerator.java
@@ -0,0 +1,53 @@
+package org.teamlaika.laikaspetpark;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.Cookie;
+
+import java.security.InvalidParameterException;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Base64;
+import java.util.Date;
+
+
+public class JwtGenerator {
+    private static byte[] ourSecret = Base64.getDecoder().decode("d6npw1rXVPd+IjWL+2uN3Hv4JcplbZx7cIJ0CMN+p6Y=");
+
+    public static String generateJwt(int userId, String role) {
+        Instant now = Instant.now();
+
+        String jwtToken = Jwts.builder()
+                .subject(Integer.toString((userId)))
+                .claim("role", role)
+                .issuedAt(Date.from(now)).expiration(Date.from(now.plus(15, ChronoUnit.MINUTES)))
+                .signWith(Keys.hmacShaKeyFor(ourSecret))
+                .compact();
+
+        //System.out.println(jwtToken);
+
+        return jwtToken;
+    }
+
+    public static Object verifyAndGetUserAndRole(String jwtToken) {
+
+
+        try {
+            Jws<Claims> result = Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(ourSecret))
+                    .build().parseSignedClaims(jwtToken);
+
+            String role = (String) result.getPayload().get("role");
+            String userId = result.getPayload().getSubject();
+            UserAndRole token = new UserAndRole(role,userId);
+            return token;
+        } catch (Exception err) {
+            throw new InvalidParameterException("JWT Validation Failed: " + err.getMessage());
+        }
+    }
+
+    // TODO: Stretch add second jwt generation and db storage for refresh token
+}
