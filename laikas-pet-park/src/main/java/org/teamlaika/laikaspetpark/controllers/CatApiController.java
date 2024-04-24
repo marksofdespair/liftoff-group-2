1a46f6bd08928f4f61cb689160f934e78b3a5cdf
Will S <wssommerhauser@gmail.com>
2024-04-23 19:57:42 -0500

    Pet Info, Provider skills list, API Crossorigins

--- laikas-pet-park/src/main/java/org/teamlaika/laikaspetpark/controllers/CatApiController.java
+++ laikas-pet-park/src/main/java/org/teamlaika/laikaspetpark/controllers/CatApiController.java
@@ -1,14 +1,12 @@
 package org.teamlaika.laikaspetpark.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.teamlaika.laikaspetpark.models.CatApi;
 import org.teamlaika.laikaspetpark.models.DogApi;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/cat-breeds")
 public class CatApiController {
